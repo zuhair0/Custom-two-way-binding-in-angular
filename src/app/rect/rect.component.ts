62a0f530f0aa6ae48018d4e782d907a53e8720b1
HP <zuhairsaleem343@gmail.com>
2025-03-13 21:03:12 +0500

    two-way custom bindin using signals

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -10,10 +10,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class RectComponent {
   // Todo: Implement custom two-way binding
   // by the combination of @input and @Output we crate the 'size' property two-way bindable
-  @Input({required: true}) size!: {width: string; height: string}
-  @Output() sizeChange = new EventEmitter<{width: string; height: string}>() // the 'Change' keyword is important as it tells angular that this @OutPut and @Input with same name are two-way bindable
+  // @Input({required: true}) size!: {width: string; height: string}
+  // @Output() sizeChange = new EventEmitter<{width: string; height: string}>() // the 'Change' keyword is important as it tells angular that this @OutPut and @Input with same name are two-way bindable
+  
+  // we can do custom two-way binding by below code
+  size = model.required<{width: string; height: string}>();
   onReset() {
-    this.sizeChange.emit({
+    // commented code is for the @Inpput and @Output
+    // this.sizeChange.emit({
+    //   width: '200',
+    //   height: '100'
+    // })
+
+    // below code is for the signals
+    this.size.set({
       width: '200',
       height: '100'
     })
